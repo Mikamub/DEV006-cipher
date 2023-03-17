7e9f95fdfa962b60942c78f4e2130562f2c4b002
Mikaella Muñoz <mikamub@gmail.com>
2023-03-17 10:45:36 -0300

    html/css completos, js encode listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,29 @@
 const cipher = {
-  // ...
+  
+  encode: function(word,offset){
+    //console.log(word,offset);
+    let wordFinal = "";
+    for (let i= 0; i<word.length; i++)
+    {
+     let letterToNumber = parseInt(word.charCodeAt(i));
+     let numberOffset = letterToNumber + offset;
+     if(numberOffset>90) {
+      let resta = numberOffset - 65;
+      let residuo = resta%26;
+      numberOffset = residuo + 65;
+     }
+     //console.log(numberOffset);
+     let encodeLetter = String.fromCharCode(numberOffset); 
+     //console.log(encodeLetter);
+      wordFinal += encodeLetter;
+    }
+    return wordFinal;
+    },
+    decode: function(word1,offset){
+      console.log(word1);
+      console.log(offset);
+    }   
 };
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let word;
+let offset;
+const barValue = document.getElementById("bar");
+const enter = document.getElementById("sendcode");
+enter.addEventListener("click", function(){
+word  = document.getElementById("code").value;
+//console.log(word);
 
-let answer;
-const putincode = document.getElementById("code");
-putincode.addEventListener("click", function(){
+offset = parseInt(barValue.value);
 
-})
+let wordEncode= cipher.encode(word,offset);
+console.log(wordEncode);
+});
+
+let word1;
+const enterCode = document.getElementById("sendcodeout");
+enterCode.addEventListener("click", function(){
+word1 = document.getElementById("code").value
+
+offset = parseInt(barValue.value);
+
+let wordDecode = cipher.decode(word1,offset);
+console.log(wordDecode);
+//console.log(word1);
+//console.log(offset);
+});
+// barValue.addEventListener("input", function(){
+// offset = barValue.getElementById("bar").value
+// console.log(offset)
+// });
