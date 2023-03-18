d552cb8a1c31e43d6990d465269f24d60bc81c46
Mikaella Muñoz <mikamub@gmail.com>
2023-03-17 21:44:18 -0300

    encode y decode listos, y test listos

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,49 @@
 const cipher = {
-  
-  encode: function(word,offset){
+
+  encode: function (offset,word) {
     //console.log(word,offset);
-    let wordFinal = "";
-    for (let i= 0; i<word.length; i++)
+    if(offset==null || word=="")
     {
-     let letterToNumber = parseInt(word.charCodeAt(i));
-     let numberOffset = letterToNumber + offset;
-     if(numberOffset>90) {
-      let resta = numberOffset - 65;
-      let residuo = resta%26;
-      numberOffset = residuo + 65;
-     }
-     //console.log(numberOffset);
-     let encodeLetter = String.fromCharCode(numberOffset); 
-     //console.log(encodeLetter);
-      wordFinal += encodeLetter;
+      throw TypeError('La funcion debe recibir numeros y palabras');
     }
-    return wordFinal;
-    },
-    decode: function(word1,offset){
-      console.log(word1);
-      console.log(offset);
-    }   
+    let wordFinalEncode = "";
+    for (let i = 0; i < word.length; i++) {
+      let letterToNumber = parseInt(word.charCodeAt(i));
+      let numberOffset = letterToNumber + offset;
+      if (numberOffset > 90) {
+        let resta = numberOffset - 65;
+        let residuo = resta % 26;
+        numberOffset = residuo + 65;
+      }
+      //console.log(numberOffset);
+      let encodeLetter = String.fromCharCode(numberOffset);
+      //console.log(encodeLetter);
+      wordFinalEncode += encodeLetter;
+    }
+    return wordFinalEncode;
+  },
+  decode: function (offset,word1) {
+    //console.log(word1);
+    // console.log(offset);
+    if(offset==null || word1=="")
+    {
+      throw TypeError('La funcion debe recibir numeros y palabras');
+    }
+    let wordFinalDecode = "";
+    for (let a = 0; a < word1.length; a++) {
+      let codeToNumber = parseInt(word1.charCodeAt(a));
+      let restaOffset = codeToNumber - offset
+      if (restaOffset < 65) {
+        let resta90 = 90 - restaOffset
+        let residuo = resta90 % 26
+        restaOffset = 90 - residuo
+        //console.log(restaOffset)
+      }
+      let decodeLetter = String.fromCharCode(restaOffset);
+      wordFinalDecode += decodeLetter
+    }
+    return wordFinalDecode;
+  }
 };
 
 
--- src/index.js
+++ src/index.js
@@ -4,28 +4,29 @@ let word;
 let offset;
 const barValue = document.getElementById("bar");
 const enter = document.getElementById("sendcode");
-enter.addEventListener("click", function(){
-word  = document.getElementById("code").value;
-//console.log(word);
+enter.addEventListener("click", function () {
+    word = document.getElementById("code").value;
+    //console.log(word);
 
-offset = parseInt(barValue.value);
+    offset = parseInt(barValue.value);
 
-let wordEncode= cipher.encode(word,offset);
-console.log(wordEncode);
+    let wordEncode = cipher.encode(offset,word);
+    console.log(wordEncode);
 });
 
 let word1;
 const enterCode = document.getElementById("sendcodeout");
-enterCode.addEventListener("click", function(){
-word1 = document.getElementById("code").value
+enterCode.addEventListener("click", function () {
+    word1 = document.getElementById("code").value
 
-offset = parseInt(barValue.value);
+    offset = parseInt(barValue.value);
 
-let wordDecode = cipher.decode(word1,offset);
-console.log(wordDecode);
-//console.log(word1);
-//console.log(offset);
+    let wordDecode = cipher.decode(offset,word1);
+    console.log(wordDecode);
+    //console.log(word1);
+    //console.log(offset);
 });
+
 // barValue.addEventListener("input", function(){
 // offset = barValue.getElementById("bar").value
 // console.log(offset)
