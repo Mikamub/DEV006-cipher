528a97a7836f2390c860e4f8bc05d46874242cae
Mika <mikamub@gmail.com>
2023-08-25 10:42:04 -0400

    "readme Actualizado"

--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,49 @@
-const cipher = {
+const cipher = {   // El objeto "cipher" contiene dos funciones: "encode" para cifrar y "decode" para descifrar.
 
-  encode: function (offset,word) {
-    //console.log(word,offset);
-    if(offset==null || word=="")
+  encode: function (offset,word) {  // La función "encode" recibe dos parámetros: "offset" (número de desplazamiento) y "word" (palabra a cifrar).
+    // Si "offset" es nulo o "word" está vacía, se lanzará un error con un mensaje.
+
+    if(offset === null || word ==="")
     {
       throw TypeError('La funcion debe recibir numeros y palabras');
     }
-    let wordFinalEncode = "";
-    for (let i = 0; i < word.length; i++) {
-      let letterToNumber = parseInt(word.charCodeAt(i));
-      let numberOffset = letterToNumber + offset;
-      if (numberOffset > 90) {
-        let resta = numberOffset - 65;
-        let residuo = resta % 26;
-        numberOffset = residuo + 65;
+    let wordFinalEncode = "";  // Se crea una variable "wordFinalEncode" para almacenar la palabra cifrada.
+    for (let i = 0; i < word.length; i++) {  // Se inicia un bucle que recorre cada carácter de la palabra "word".
+      const letterToNumber = parseInt(word.charCodeAt(i));    // Se obtiene el valor numérico del carácter actual en la posición "i".
+      let numberOffset = letterToNumber + offset; // Se calcula el nuevo valor numérico sumando el desplazamiento "offset".
+      if (numberOffset > 90) {   // Si el nuevo valor numérico supera el código ASCII de la letra 'Z' (90),
+        // significa que se ha desplazado más allá del final del alfabeto en mayúsculas.
+        const resta = numberOffset - 65; // Se calcula la diferencia entre el nuevo valor y el código ASCII de la letra 'A' (65).
+        const residuo = resta % 26; // Se calcula el residuo de la diferencia al dividir entre 26, ya que hay 26 letras en el alfabeto.
+        numberOffset = residuo + 65; // El nuevo valor numérico se ajusta para que vuelva a estar dentro del rango de letras mayúsculas.
       }
-      //console.log(numberOffset);
-      let encodeLetter = String.fromCharCode(numberOffset);
-      //console.log(encodeLetter);
-      wordFinalEncode += encodeLetter;
+      const encodeLetter = String.fromCharCode(numberOffset);// Se convierte el valor numérico cifrado nuevamente a su carácter correspondiente.
+      wordFinalEncode += encodeLetter; // Se agrega el carácter cifrado a la variable "wordFinalEncode".
     }
     return wordFinalEncode;
+    // Se retorna la palabra cifrada.
   },
-  decode: function (offset,word1) {
-    //console.log(word1);
-    // console.log(offset);
-    if(offset==null || word1=="")
+  decode: function (offset,word1) {// La función "decode" recibe dos parámetros: "offset" (número de desplazamiento) y "word1" (palabra a descifrar).
+    // Si "offset" es nulo o "word1" está vacía, se lanzará un error con un mensaje.
+    if(offset === null || word1 === "")
     {
       throw TypeError('La funcion debe recibir numeros y palabras');
     }
-    let wordFinalDecode = "";
-    for (let a = 0; a < word1.length; a++) {
-      let codeToNumber = parseInt(word1.charCodeAt(a));
-      let restaOffset = codeToNumber - offset
-      if (restaOffset < 65) {
-        let resta90 = 90 - restaOffset
-        let residuo = resta90 % 26
-        restaOffset = 90 - residuo
-        //console.log(restaOffset)
+    let wordFinalDecode = "";  // Se crea una variable "wordFinalDecode" para almacenar la palabra descifrada.
+    for (let a = 0; a < word1.length; a++) { // Se inicia un bucle que recorre cada carácter de la palabra "word1".
+      const codeToNumber = parseInt(word1.charCodeAt(a));  // Se obtiene el valor numérico del carácter actual en la posición "a".
+      let restaOffset = codeToNumber - offset // Se calcula el nuevo valor numérico restando el desplazamiento "offset".
+      if (restaOffset < 65) { // Si el nuevo valor numérico es menor que el código ASCII de la letra 'A' (65),
+        // significa que se ha desplazado más allá del inicio del alfabeto en mayúsculas.
+        const resta90 = 90 - restaOffset // Se calcula la diferencia entre el código ASCII de la letra 'Z' (90) y el nuevo valor numérico.
+        const residuo = resta90 % 26  // Se calcula el residuo de la diferencia al dividir entre 26, ya que hay 26 letras en el alfabeto.
+        restaOffset = 90 - residuo // El nuevo valor numérico se ajusta para que vuelva a estar dentro del rango de letras mayúsculas.
       }
-      let decodeLetter = String.fromCharCode(restaOffset);
-      wordFinalDecode += decodeLetter
+      
+      const decodeLetter = String.fromCharCode(restaOffset);  // Se convierte el valor numérico descifrado nuevamente a su carácter correspondiente.
+      wordFinalDecode += decodeLetter  // Se agrega el carácter descifrado a la variable "wordFinalDecode".
     }
-    return wordFinalDecode;
+    return wordFinalDecode;  // Se retorna la palabra descifrada.
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 let word;
 let offset;
@@ -6,30 +6,26 @@ const result = document.getElementById("resultado Encode");
 const barValue = document.getElementById("bar");
 const enter = document.getElementById("sendcode");
 enter.addEventListener("click", function () {
-    word = document.getElementById("code").value;
-    //console.log(word);
+  word = document.getElementById("code").value;
 
-    offset = parseInt(barValue.value);
+  offset = parseInt(barValue.value);
 
-    let wordEncode = cipher.encode(offset,word);
-    console.log(wordEncode);
-    let resultEncode =  " " + wordEncode;
-    result.innerHTML=resultEncode;
+  const wordEncode = cipher.encode(offset, word);
+  console.log(wordEncode);
+  const resultEncode = " " + wordEncode;
+  result.innerHTML = resultEncode;
 });
-
 
 const result1 = document.getElementById("resultado Decode");
 let word1;
 const enterCode = document.getElementById("sendcodeout");
 enterCode.addEventListener("click", function () {
-    word1 = document.getElementById("code").value
+  word1 = document.getElementById("code").value;
 
-    offset = parseInt(barValue.value);
+  offset = parseInt(barValue.value);
 
-    let wordDecode = cipher.decode(offset,word1);
-    console.log(wordDecode);
-    let resultDecode =  " " + wordDecode;
-    result1.innerHTML=resultDecode;
-    //console.log(word1);
-    //console.log(offset);
+  const wordDecode = cipher.decode(offset, word1);
+  console.log(wordDecode);
+  const resultDecode = " " + wordDecode;
+  result1.innerHTML = resultDecode;
 });
